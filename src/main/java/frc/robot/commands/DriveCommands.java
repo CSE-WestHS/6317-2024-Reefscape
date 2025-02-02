ba07a878372f4d3e5c9ecd35b93941146dd0d0be
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2025-02-02 10:39:11 -0600

    Field Relative fix

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -80,7 +80,7 @@ public class DriveCommands {
           boolean isFlipped =
               DriverStation.getAlliance().isPresent()
                   && DriverStation.getAlliance().get() == Alliance.Red;
-          ChassisSpeeds.fromFieldRelativeSpeeds(
+          speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
               speeds,
               isFlipped ? drive.getRotation().plus(new Rotation2d(Math.PI)) : drive.getRotation());
           drive.runVelocity(speeds);
@@ -129,7 +129,7 @@ public class DriveCommands {
               boolean isFlipped =
                   DriverStation.getAlliance().isPresent()
                       && DriverStation.getAlliance().get() == Alliance.Red;
-              ChassisSpeeds.fromFieldRelativeSpeeds(
+              speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
                   speeds,
                   isFlipped
                       ? drive.getRotation().plus(new Rotation2d(Math.PI))
