59e9e44c26cb0c312a4d560ad6dae9039e61c361
6317DS1 <162824044+6317-Disruptive-Innovation@users.noreply.github.com>
2025-02-17 15:55:54 -0600

    LED Logic Start

--- src/main/java/frc/robot/subsystems/LEDS/LEDS.java
+++ src/main/java/frc/robot/subsystems/LEDS/LEDS.java
@@ -4,43 +4,49 @@
 
 package frc.robot.subsystems.LEDS;
 
-import java.util.Map;
 
-import edu.wpi.first.units.TimeUnit;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
-import frc.robot.RobotContainer;
+import frc.robot.util.ButtonBoardButtons;
 
 /** Add your docs here. */
 public class LEDS {
     AddressableLED led;
     AddressableLEDBuffer ledbuffer;
-    AddressableLEDBuffer buff2;
     LEDPattern red = LEDPattern.solid(Color.kRed);
     LEDPattern green = LEDPattern.solid(Color.kGreen);
-    LEDPattern f = LEDPattern.rainbow(150, 150);
     LEDPattern cyan = LEDPattern.solid(Color.kDarkCyan);
     
     public LEDS(int length) {
         led = new AddressableLED(0);
         ledbuffer = new AddressableLEDBuffer(length);
         led.setLength(ledbuffer.getLength());
-        // buff2 = new AddressableLEDBuffer(length/2);
         
 
         led.setData(ledbuffer);
         led.start();
     }
     public void runLEDS() {
-        if (DriverStation.isEStopped()) {
+        if (ButtonBoardButtons.LEVEL_1.getAsBoolean()) {
+            red.applyTo(ledbuffer);
+            led.setData(ledbuffer);
+        }
+        if (ButtonBoardButtons.LEVEL_2.getAsBoolean()) {
+            green.applyTo(ledbuffer);
+            led.setData(ledbuffer);
+        }
+        if (ButtonBoardButtons.LEVEL_3.getAsBoolean()) {
+            cyan.applyTo(ledbuffer);
+            led.setData(ledbuffer);
+        }
+        if (DriverStation.isDisabled()) {
             for (int i = 0; i < ledbuffer.getLength()/2; ++i) {
                 ledbuffer.setLED(i, Color.kGreen);
             }
+            
             for (int i = ledbuffer.getLength() / 2; i < ledbuffer.getLength(); ++i) {
                 ledbuffer.setLED(i, Color.kCyan);
             }
